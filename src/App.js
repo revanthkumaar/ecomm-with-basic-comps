74cb22a9dddd3846db8213ccb5f9ad9ee4c3f6a9
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-11 10:49:56 +0530

    lakjlk
    jslkjals

--- src/App.js
+++ src/App.js
@@ -7,13 +7,44 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+
 function App() {
+
+  componentDidMount(){
+
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+
+      setCurrentUser(userAuth);
+    });
+  }
+
+
+
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route 
+          exact
+          path='signin'
+          render={()=>{this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}}
+        />
       </Switch>
     </div>
   );
